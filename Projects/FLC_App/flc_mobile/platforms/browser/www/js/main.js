82e0ac5a2c20b3afb97f39de289e64a1612f06f1
AstroBoogie <nathaniel.adams.us@gmail.com>
2016-11-08 17:41:18 -0800

    fixed schedule content css formatting

--- Projects/FLC_App/flc_mobile/platforms/browser/www/js/main.js
+++ Projects/FLC_App/flc_mobile/platforms/browser/www/js/main.js
@@ -1,12 +1,223 @@
-var body = $("#body-container");
+// // Disable Horizontal Scrolling
+// $(function() {
+//
+//     var $body = $(document);
+//     $body.bind('scroll', function() {
+//         // "Disable" the horizontal scroll.
+//         if ($body.scrollLeft() !== 0) {
+//             $body.scrollLeft(0);
+//         }
+//     });
+//
+// });
+
+// fast Click
+$(function() {
+    FastClick.attach(document.body);
+});
+
+// Header Slider
+var body = $(".body-container");
 var header = $("#header-container");
 
 body.scroll(function () {
+    var scrollText = $("#counter");
+    var currPos = this.scrollTop;
 
-    if (this.scrollTop > 50) {
+    scrollText.text(currPos);
+
+    if (this.scrollTop > 62) {
         header.addClass("header-scaled");
     } else {
         header.removeClass("header-scaled");
     }
 
+});
+
+// Subject Button Click
+var subjectButton = $("#subject");
+var scheduleContainer = $("#schedule-container");
+var scheduleCategories = $("#schedule-categories");
+// $("#category-back").prop('disabled', true);
+
+// creates subject debounce
+var subjectPage = false;
+
+var selectedVal;
+
+// Creates function to slide schedule page
+var categorySlide = function () {
+    scheduleContainer.addClass("container-scaled");
+    scheduleCategories.addClass("categories-scaled");
+
+    // $("#category-back").prop('disabled', false);
+    // $("#category-back").addClass("enable-opacity");
+
+};
+
+// Creates function that unslides schedule page
+var categoryUnslide = function () {
+    $("input").prop('disabled', true);
+    scheduleContainer.removeClass("container-scaled");
+    scheduleCategories.removeClass("categories-scaled");
+    // $("#category-back").removeClass("enable-opacity");
+
+    $(".body-container").scrollTop(0);
+};
+
+// runs categorySlide when subjectButton is clicked
+subjectButton.click(function () {
+    subjectPage = true;
+    categorySlide();
+});
+
+// runs categoryUnslide when back button is pressed
+$("#category-back").click(function () {
+    if (subjectPage == true) {
+        subjectPage = false;
+        categoryUnslide();
+    }
+    else {
+        window.location = "index.html";
+    }
+});
+
+// Gets which week radio button is checked, adds background and gets value
+var totalLabels = $("#weekdays").children("form").children("label");
+
+$('#weekdays input').on('change', function() {
+    var selectionLabel = $(this).parent().children("span");
+
+    if (selectionLabel.css('background-color') == "rgb(220, 220, 220)") {
+        console.log(selectionLabel.css('background-color'));
+        selectionLabel.css('background-color', 'rgb(255, 255, 255)');
+    }
+    else {
+        console.log(selectionLabel.css('background-color'));
+        selectionLabel.css('background-color', 'rgb(220, 220, 220)');
+    }
+});
+
+// Configure/Contents Clicker
+var configureButton = $("#configure");
+var contentsButton = $("#contents");
+
+var configureContainer = $("#configure-container");
+var courseContainer = $("#course-container");
+
+var confToggled = true;
+
+var clickToggle = function(clicked, unclicked, configure, courses) {
+    clicked.addClass("category-selected");
+    unclicked.removeClass("category-selected");
+
+    configureContainer.removeClass();
+    courseContainer.removeClass();
+
+    configureContainer.addClass(configure);
+    courseContainer.addClass(courses);
+};
+
+configureButton.click(function () {
+    if (confToggled == false) {
+        confToggled = true;
+        clickToggle(configureButton, contentsButton, "category-zero", "category-push");
+    }
+});
+
+contentsButton.click(function () {
+    if (confToggled == true) {
+        confToggled = false;
+        clickToggle(contentsButton, configureButton, "category-pull", "category-zero");
+    }
+});
+
+// organize items dictionary (example currently)
+var course = {};
+
+// creates list of course titles
+var courseTitles = [];
+
+$.getJSON("classesOut.json", function(data) {
+
+    // creates course list
+    $.each(data, function(index, element) {
+        $(element).each(function (i) {
+            // creates course list
+            course["course" + i] = element[i];
+
+            // creates course title
+            var selectedTitle = element[i]["courseTitle"];
+            var titleName = selectedTitle.split(" ")[0];
+            if (courseTitles.indexOf(titleName) == -1) {
+                courseTitles.push(titleName);
+
+                // creates schedule categories
+                $("#schedule-categories").append("<button class='category-container'><div class='category-text'><span>" + titleName + "</span></div></button>")
+            }
+            else {
+            }
+        });
+    });
+
+    // create schedule categories
+
+
+
+
+
+    // organize courses dictionary into course container objects
+    // for (i = 0, i < data.length, i++) {
+    //     $( "<div/>", {
+    //         class: "course-title",
+    //         text: (course["course" + i][0] + course["course" + i][1])
+    //     }).appendTo("#course-container");
+    // }
+
+})
+.done(function() {
+
+    var categoryButton = $(".category-container");
+    var subjectText = $("#subject-text");
+
+    // Updates text values when one of the categories in the subject list is clicked
+    categoryButton.click(function () {
+        selectedVal = $(this).children('div').children('span').text();
+        subjectText.text(selectedVal);
+
+        subjectPage = false;
+        categoryUnslide();
+
+    });
+
+    // Star Click
+    var starButton = $(".category-star");
+
+    starButton.click(function () {
+        console.log("clicked");
+        $(this).children('i').toggleClass("starToggle");
+    });
+
+    // updates course listing on courses button click
+    $("#contents").click(function() {
+        $("#course-container").empty();
+
+        for (var key in course) {
+            var title = course[key]["courseTitle"];
+            var className = course[key]["courseName"];
+
+            var tempCourseTitle = title.split(" ")[0];
+
+            if (tempCourseTitle == selectedVal) {
+                $("#course-container").append("<div class='category' id='" + key + "'><div class='category-body'><div class='category-header'><span class='course-title'>" + title + " " + className + "</span></div><div class='category-description'><span class='course-days'>MoWeFri</span><span class='course-time'>9:00am - 10:20am</span></div></div><button class='category-star'><i class='material-icons'>stars</i></button></div>")
+
+                var currContainer = $("#" + key)
+
+                var containerHeight = currContainer.find(".category-body").height();
+
+                currContainer.find(".category-star").height(containerHeight);
+
+            }
+        }
+    })
 });
--- Projects/FLC_App/flc_mobile/www/js/main.js
+++ Projects/FLC_App/flc_mobile/www/js/main.js
@@ -11,6 +11,11 @@
 //
 // });
 
+// fast Click
+$(function() {
+    FastClick.attach(document.body);
+});
+
 // Header Slider
 var body = $(".body-container");
 var header = $("#header-container");
@@ -18,7 +23,6 @@ var header = $("#header-container");
 body.scroll(function () {
     var scrollText = $("#counter");
     var currPos = this.scrollTop;
-    console.log(currPos);
 
     scrollText.text(currPos);
 
@@ -38,6 +42,8 @@ var scheduleCategories = $("#schedule-categories");
 
 // creates subject debounce
 var subjectPage = false;
+
+var selectedVal;
 
 // Creates function to slide schedule page
 var categorySlide = function () {
@@ -76,29 +82,20 @@ $("#category-back").click(function () {
     }
 });
 
-var categoryButton = $(".category-container");
-var subjectText = $("#subject-text");
-var selectedVal;
-
-// Updates text values when one of the categories in the subject list is clicked
-categoryButton.click(function () {
-    selectedVal = $(this).children('div').children('span').text();
-    subjectText.text(selectedVal);
-
-    subjectPage = false;
-    categoryUnslide();
-});
-
 // Gets which week radio button is checked, adds background and gets value
 var totalLabels = $("#weekdays").children("form").children("label");
 
 $('#weekdays input').on('change', function() {
-    totalLabels.css('background-color', 'white');
-    
-    var selectionLabel = $(this).parent().parent();
-    selectionLabel.css('background-color', '#EDEDED');
+    var selectionLabel = $(this).parent().children("span");
 
-    console.log($(this).prop("value"));
+    if (selectionLabel.css('background-color') == "rgb(220, 220, 220)") {
+        console.log(selectionLabel.css('background-color'));
+        selectionLabel.css('background-color', 'rgb(255, 255, 255)');
+    }
+    else {
+        console.log(selectionLabel.css('background-color'));
+        selectionLabel.css('background-color', 'rgb(220, 220, 220)');
+    }
 });
 
 // Configure/Contents Clicker
@@ -135,65 +132,92 @@ contentsButton.click(function () {
     }
 });
 
-// Star Click
-var starButton = $(".category-star");
+// organize items dictionary (example currently)
+var course = {};
 
-starButton.click(function () {
-    console.log("cat");
-    $(this).children('i').toggleClass("starToggle");
-});
+// creates list of course titles
+var courseTitles = [];
 
-// Append course data to generated elements
+$.getJSON("classesOut.json", function(data) {
 
-var courseData = "https://cdn.rawgit.com/AstroBoogie/DataScience/master/Projects/FLC_App/app_data/classesOut.json";
-var items = [];
+    // creates course list
+    $.each(data, function(index, element) {
+        $(element).each(function (i) {
+            // creates course list
+            course["course" + i] = element[i];
 
-var success = function () {
-    console.log("cat");
-};
+            // creates course title
+            var selectedTitle = element[i]["courseTitle"];
+            var titleName = selectedTitle.split(" ")[0];
+            if (courseTitles.indexOf(titleName) == -1) {
+                courseTitles.push(titleName);
 
-var jqxhr = $.getJSON( "classesOut.json", function() {
-    console.log( "success" );
-})
-    .done(function() {
-        console.log( "second success" );
-    })
-    .fail(function() {
-        console.log( "error" );
-    })
-    .always(function() {
-        console.log( "complete" );
+                // creates schedule categories
+                $("#schedule-categories").append("<button class='category-container'><div class='category-text'><span>" + titleName + "</span></div></button>")
+            }
+            else {
+            }
+        });
     });
 
-// Perform other work here ...
+    // create schedule categories
 
 
-// $.getJSON("blabla.json", function(data) {
-//
-//     // loop through data, parse it into items dictionary
-//
-//     // organize items dictionary (example currently)
-//     var course = {
-//         course1 : [
-//             "PHIL 300",
-//             "Introduction to Philosophy",
-//             "MoWeFri",
-//             "9:00am - 10:20am"
-//         ],
-//         course2 : [
-//             "PHIL 310",
-//             "Introduction to Ethics",
-//             "TuThu",
-//             "1:00pm - 3:00pm"
-//         ]
-//     };
-//
-//     // organize courses dictionary into course container objects
-//     for (i = 0, i < data.length, i++) {
-//         $( "<div/>", {
-//             class: "course-title",
-//             text: (course["course" + i][0] + course["course" + i][1])
-//         }).appendTo("#course-container");
-//     }
-//
-// });
+
+
+
+    // organize courses dictionary into course container objects
+    // for (i = 0, i < data.length, i++) {
+    //     $( "<div/>", {
+    //         class: "course-title",
+    //         text: (course["course" + i][0] + course["course" + i][1])
+    //     }).appendTo("#course-container");
+    // }
+
+})
+.done(function() {
+
+    var categoryButton = $(".category-container");
+    var subjectText = $("#subject-text");
+
+    // Updates text values when one of the categories in the subject list is clicked
+    categoryButton.click(function () {
+        selectedVal = $(this).children('div').children('span').text();
+        subjectText.text(selectedVal);
+
+        subjectPage = false;
+        categoryUnslide();
+
+    });
+
+    // Star Click
+    var starButton = $(".category-star");
+
+    starButton.click(function () {
+        console.log("clicked");
+        $(this).children('i').toggleClass("starToggle");
+    });
+
+    // updates course listing on courses button click
+    $("#contents").click(function() {
+        $("#course-container").empty();
+
+        for (var key in course) {
+            var title = course[key]["courseTitle"];
+            var className = course[key]["courseName"];
+
+            var tempCourseTitle = title.split(" ")[0];
+
+            if (tempCourseTitle == selectedVal) {
+                $("#course-container").append("<div class='category' id='" + key + "'><div class='category-body'><div class='category-header'><span class='course-title'>" + title + " " + className + "</span></div><div class='category-description'><span class='course-days'>MoWeFri</span><span class='course-time'>9:00am - 10:20am</span></div></div><button class='category-star'><i class='material-icons'>stars</i></button></div>")
+
+                var currContainer = $("#" + key)
+
+                var containerHeight = currContainer.find(".category-body").height();
+
+                currContainer.find(".category-star").height(containerHeight);
+
+            }
+        }
+    })
+});
