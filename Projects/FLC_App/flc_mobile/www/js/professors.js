c529d66b69f8a2ddb88f6bf2d04a766def5e6445
Aaron Hartigan <hartiganaaron@gmail.com>
2016-12-28 10:27:58 -0800

    Refactoring

--- Projects/FLC_App/flc_mobile/www/js/professors.js
+++ Projects/FLC_App/flc_mobile/www/js/professors.js
@@ -1,68 +1,79 @@
-var instructors;
 var currentPage = "search";
 var backSelectable = true;
 
 var searchText = $("#search-text");
 var searchInput = $("#search-text > input");
 
-var professorUpdate = function (professors) {
-    //$("#professor-container").empty();
+// Creates a div for each professor and appends it to the
+// professor-container div.
+var createProfessors = function (professors) {
     $.each(professors, function(index, element) {
-        let professorName = element["name"]
-        let professorEmail = element["email"] || ""
-        let professorPhone = element["phone"] || ""
-        let professorSubjects
-        // If the professor has too many subjects, limit their list to 3
-        if (element["subjects"].length > 3) {
-            element["subjects"] = element["subjects"].slice(0, 3)
-            professorSubjects = element["subjects"].join(", ") + "…";
-        }
-        else {
-            professorSubjects = element["subjects"].join(", ");
-        }
-        let professorStatus = ""
-        let professorHours = element["classHours"];
-        professorHours = parseInt(professorHours.substring(0, professorHours.indexOf('hr')));
-        if (professorHours >= 8 || element["classList"].length >= 6) {
-            professorStatus = "Full-time";
-        }
-        else {
-            professorStatus = "Part-time";
-        }
+        let name = element["name"];
+        let email = element["email"] || "";
+        let phone = element["phone"] || "";
+        let subjects = createSubjectList(element["subjects"], 3);
+        let status = determineProfessorStatus(element["classHours"], element["classList"]);
 
-        $("#professor-container").append("\
-            <div class='course-object'>\
-                <div class='course-text-container'>\
-                    <div class='professor-title'>\
-                        <span>" + professorName + "</span>\
-                    </div>\
-                    <div class='professor-info'>\
-                        <div class='professor-status'>\
-                            <span>" + professorStatus + "</span>\
-                        </div>\
-                        <div class='professor-subject'>\
-                            <span>" + professorSubjects + "</span>\
-                        </div>\
-                    </div>\
-                    <div class='professor-info'>\
-                        <span>" + professorEmail + "</span>\
-                    </div>\
-                    <div class='professor-info'>\
-                        <span>" + professorPhone + "</span>\
-                    </div>\
-                </div>\
-                <div class='professor-arrow'>\
-                    <i class='material-icons'>keyboard_arrow_right</i>\
-                </div>\
-            </div>");
+        $("#professor-container").append(professorInfo(name, status, subjects, email, phone));
     });
 
+    // Adds professor-arrow click-functionality to view
+    // more in-depth professor information.
     $(".professor-arrow").click(function () {
         backSelectable = true;
         currentPage = "professor-overlay";
         professorPage(this);
     });
 };
+
+// Returns a string representation of a list of subjects
+// up to the designated maximum.
+var createSubjectList = function(subjects, max = 99) {
+    if (subjects.length > max) {
+        return subjects.slice(0, max).join(", ") + "…";
+    }
+    return subjects.join(", ");
+}
+
+// Returns a string indicating the professors status
+// hours is passed as a string in the form "8hr 50min"
+var determineProfessorStatus = function(hours, classlist) {
+    let parsedHours = parseInt(hours.substring(0, hours.indexOf('hr')));
+    if (parsedHours >= 8 || classlist.length >= 6) {
+        return "Full-time";
+    }
+    return "Part-time";
+}
+
+// Returns a div outlining the professor's information
+var professorInfo = function(name, status, subjects, email, phone) {
+    return ("\
+        <div class='course-object'>\
+            <div class='course-text-container'>\
+                <div class='professor-title'>\
+                    <span>" + name + "</span>\
+                </div>\
+                <div class='professor-info'>\
+                    <div class='professor-status'>\
+                        <span>" + status + "</span>\
+                    </div>\
+                    <div class='professor-subject'>\
+                        <span>" + subjects + "</span>\
+                    </div>\
+                </div>\
+                <div class='professor-info'>\
+                    <span>" + email + "</span>\
+                </div>\
+                <div class='professor-info'>\
+                    <span>" + phone + "</span>\
+                </div>\
+            </div>\
+            <div class='professor-arrow'>\
+                <i class='material-icons'>keyboard_arrow_right</i>\
+            </div>\
+        </div>"
+    );
+}
 
 var backArrowPress = function(pageSet) {
     if (backSelectable == true) {
@@ -90,8 +101,6 @@ var backArrowPress = function(pageSet) {
 };
 
 var professorPage = function(object) {
-
-    console.log($(object).parent());
     var professorTitle = $(object).parent().find($(".professor-title > span")).text();
     var professorStatus = $(object).parent().find($(".professor-status")).text();
     var professorSubject = $(object).parent().find($(".professor-subject")).text();
@@ -127,7 +136,7 @@ $.ajax({
     url: url,
     type: "GET",
     success: function(data) {
-        professorUpdate(data);
+        createProfessors(data);
     },
 });
 
@@ -143,52 +152,66 @@ $(function () {
     });
 });
 
-// sets width of of text inputs to placeholder length
-$("input[placeholder]").each(function () {
-    $(this).attr('size', $(this).attr('placeholder').length);
-});
+// sets width of text inputs to placeholder length
+var setIntputTextWidth = function() {
+    $("input[placeholder]").each(function () {
+        $(this).attr('size', $(this).attr('placeholder').length);
+    });
+}
 
 // Resets container widths to pixel equivalents
-searchText.css("width", searchText.width());
+var resetContainerWidth = function() {
+    searchText.css("width", searchText.width());
+}
 
 // Creates transitions when the search bar is clicked
-$("#header-search").click(function () {
+var createSearchTransition = function() {
+    $("#header-search").click(function () {
+        backSelectable = false;
 
-    backSelectable = false;
+        $("#back-arrow").addClass("transition-back-arrow");
+        $("#header-search").addClass("transition-header-search");
+        $("#search-text-container").addClass("transition-search-text-container");
+        $("#search-icon > i").addClass("transition-search-icon");
+        $("#cancel-button-container").addClass("transition-cancel-button-container");
+        $("#cancel-button > span").addClass("transition-cancel-button");
 
-    $("#back-arrow").addClass("transition-back-arrow");
-    $("#header-search").addClass("transition-header-search");
-    $("#search-text-container").addClass("transition-search-text-container");
-    $("#search-icon > i").addClass("transition-search-icon");
-    $("#cancel-button-container").addClass("transition-cancel-button-container");
-    $("#cancel-button > span").addClass("transition-cancel-button");
-
-    searchInput.focus();
-});
+        searchInput.focus();
+    });
+}
 
 // Removes transitions when the search cancel button is clicked
-$("#cancel-button").click(function () {
+var removeSearchTransition = function() {
+    $("#cancel-button").click(function () {
+        searchInput.val("");
+        //professorUpdate("");
 
-    searchInput.val("");
-    //professorUpdate("");
+        backSelectable = true;
 
-    backSelectable = true;
-
-    $("#back-arrow").removeClass("transition-back-arrow");
-    $("#header-search").removeClass("transition-header-search");
-    $("#search-text-container").removeClass("transition-search-text-container");
-    $("#search-icon > i").removeClass("transition-search-icon");
-    $("#cancel-button-container").removeClass("transition-cancel-button-container");
-    $("#cancel-button > span").removeClass("transition-cancel-button");
-});
+        $("#back-arrow").removeClass("transition-back-arrow");
+        $("#header-search").removeClass("transition-header-search");
+        $("#search-text-container").removeClass("transition-search-text-container");
+        $("#search-icon > i").removeClass("transition-search-icon");
+        $("#cancel-button-container").removeClass("transition-cancel-button-container");
+        $("#cancel-button > span").removeClass("transition-cancel-button");
+    });
+}
 
 // Updates search results upon key press
-searchInput.keyup(function() {
-    if (searchInput.val().length > 0) {
-        //professorUpdate(searchInput.val());
-    }
+var updateSearchResults = function() {
+    searchInput.keyup(function() {
+        if (searchInput.val().length > 0) {
+            //professorUpdate(searchInput.val());
+        }
 
-    if (searchInput.val().length <= 0) {
-        //professorUpdate("");
-    }
-});
+        if (searchInput.val().length <= 0) {
+            //professorUpdate("");
+        }
+    });
+}
+
+setIntputTextWidth();
+resetContainerWidth();
+createSearchTransition();
+removeSearchTransition();
+updateSearchResults();
